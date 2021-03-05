86d5cf6120b98aa17ef652f5cebc748590c1f115
olliebrownlow <olliebrownlow@gmail.com>
2021-03-05 22:25:00 +0000

    Fixes cross origin error, changes to about.js

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 
 // functional component
-const About = () => {
-    const message = "Hello world!"
+// const About = () => {
+//   const message = "Hello world!"
+//   return (
+//     <h1>About page - {message}</h1>
+//   )
+// }
+
+class About extends React.Component {
+  render() {
     return (
-      <h1>About page - {message}</h1>
+      <h1>I am a class function</h1>
     )
+  }
 }
-    export default About
+
+export default About

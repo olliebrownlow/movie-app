2f9d2b8b1d3a587140a977733112e9583e4fe8c9
Oliver Brownlow <olliebrownlow@gmail.com>
2021-03-13 17:18:36 +0000

    formatting changes

--- pages/index.js
+++ pages/index.js
@@ -16,30 +16,21 @@ export default function Home() {
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
     </Head>
-    
     <Navbar />
- 
-  <div className="container">
-    <div className="row">
- 
-      <div className="col-lg-3">
-        <Sidebar />
-      </div>
- 
-      <div className="col-lg-9">
-      
-        <Carousel />
- 
-        <div className="row">
- 
-          <MovieList />
-          
+    <div className="container">
+      <div className="row"> 
+        <div className="col-lg-3">
+          <Sidebar />
+        </div>
+        <div className="col-lg-9">
+          <Carousel />
+          <div className="row">
+            <MovieList />
+          </div>
         </div>
       </div>
     </div>
+    <Footer />
   </div>
-  
-  <Footer />
-    </div>
   )
 }

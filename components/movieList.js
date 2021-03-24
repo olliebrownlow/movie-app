48602098dd48e211ba84f8c793e06440532b2eaa
Oliver Brownlow <olliebrownlow@gmail.com>
2021-03-24 06:32:40 +0000

    adds and displays a count state

--- components/movieList.js
+++ components/movieList.js
@@ -2,15 +2,36 @@ import React from 'react'
 
 
 class MovieList extends React.Component {
+  // state = {
+  //   count: 0
+  // }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment = () => {
+    alert("incrementing number")
+  }
+  
+  decrement = () => {
+    alert("decrementing number")
+  }
 
   render() {
     return (
       <>
         <div>
-          <button className="btn btn-primary">Increment number</button>
-          <button className="btn btn-primary">Decrement number</button>
+          <button onClick={this.increment} className="btn btn-primary">Increment number</button>
+          <button onClick={this.decrement} className="btn btn-primary">Decrement number</button>
+          
+          <h1>{this.state.count}</h1>
         </div>
-
+        
         <div className="col-lg-4   col-md-6 mb-4">
           <div className="card h-100">
             <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>

b22076f92eb1304c035eba0a834e83b5b23544e6
Oliver Brownlow <olliebrownlow@gmail.com>
2021-03-24 21:33:06 +0000

    adds state to sidebar functional component

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,4 +1,18 @@
+import { useState } from 'react'
+
 const Sidebar = () => {
+  const [ count, setCount ] = useState(0)
+
+  const increment = () => {
+    const newCount = count + 1
+    setCount(newCount)
+  }
+  
+  const decrement = () => {
+    const newCount = count - 1
+    setCount(newCount)
+  }
+
   return (
     <div>
       <h1 className="my-4">Shop Name</h1>
@@ -7,6 +21,12 @@ const Sidebar = () => {
         <a href="#" className="list-group-item">Category 2</a>
         <a href="#" className="list-group-item">Category 3</a>
       </div>
+      <div>
+        <button onClick={increment} className="btn btn-primary">Increment number</button>
+        <button onClick={decrement} className="btn btn-primary">Decrement number</button>
+          
+        <h1>{count}</h1>
+      </div>  
     </div>
   )
 }

1eca660fcc69f3be5efd83bec91f3007cd30c63d
Oliver Brownlow <olliebrownlow@gmail.com>
2021-04-16 08:55:51 +0100

    adds navigation by movie id to individual movie detail pages

---
+++ pages/movies/[id].js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRouter } from 'next/router';
+
+const Movie = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  return (
+  <div className="container">
+    <h1>Movie with id: { id }</h1>
+  </div>
+  )
+}
+
+export default Movie
+
+
